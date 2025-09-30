35c2709ddc65bff201bf2a483bbd6d8bba687466
bestcar137 <bestcar137@gmail.com>
2025-09-30 12:50:55 +0900

    last update: 2025/09/30
    - add: Chap04 prob folder with examples
      - prob01/cat.c: read and print file contents to stdout
      - prob02/copy.c: file copy implementation
      - prob03/line.c: add line numbers while printing file
      - prob04/fprint.c: save student data to text file
      - prob05/fscan.c: read and display student data from text file
      - prob06/stcreate1.c: save student data using binary format
      - prob07/stprint.c: display student records from binary file
      - prob08/buffer.c: demonstrate buffering modes
      - student.h: student structure definition
      - test.txt: test input file for prob01
    
    - update: CMakeLists.txt (added Chap04 prob executables)
    
    - note: PPT 슬라이드 예제 코드만 완성
    
    - TODO: 실습문제에서 요구하는 조건 충족 필요
       - 과제 1: 파일 추가 프로그램
       - 과제 2: cat 명령어 확장

---
+++ Chapters/chap04/prob/prob01/cat.c
@@ -0,0 +1,22 @@
+//
+// Created by bestc on 25. 9. 30..
+//
+
+#include <stdio.h>
+/* 텍스트 파일 내용을 표준출력에 출력 */
+
+int main(int argc, char *argv[]) {
+    FILE *fp;
+    int c;
+    if (argc < 2)
+        fp = stdin;
+    else fp = fopen(argv[1], "r");
+
+    c = getc(fp);
+    while (c != EOF) {
+        putc(c, stdout);
+        c = getc(fp);
+    }
+    fclose(fp);
+    return 0;
+}
---
+++ Chapters/chap04/prob/prob04/student.h
@@ -0,0 +1,14 @@
+//
+// Created by bestc on 25. 9. 30..
+//
+
+#ifndef SYSPRO_STUDENT_H
+#define SYSPRO_STUDENT_H
+
+struct student {
+    int id;
+    char name[20];
+    short score;
+};
+
+#endif //SYSPRO_STUDENT_H
